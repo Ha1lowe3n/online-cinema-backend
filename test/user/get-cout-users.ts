55271e616ed0779a02c374f52e9281bbdd2cc788
Igor Trifonov <Ha1lowe3n@yandex.ru>
2022-04-22 14:55:02 +0800

    e2e tested get count users

---
+++ test/user/get-cout-users.ts
@@ -0,0 +1,83 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { TestingModule, Test } from '@nestjs/testing';
+import * as request from 'supertest';
+import { sign } from 'jsonwebtoken';
+import { config } from 'dotenv';
+import { Types } from 'mongoose';
+
+config();
+
+import { UserErrorMessages } from '../../src/utils/error-messages/user-error-messages';
+import { CommonErrorMessages } from './../../src/utils/error-messages/common-error-messages';
+import { AuthErrorMessages } from './../../src/utils/error-messages/auth-error-messages';
+
+import { MockAppModule } from '../mock-app.module';
+import { testAdminUser, testNewUser } from './data';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const usersGetUsersCount = () => {
+	let app: INestApplication;
+	let adminToken: string;
+	let userToken: string;
+
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [MockAppModule],
+		}).compile();
+
+		app = moduleFixture.createNestApplication();
+		app.useGlobalPipes(new ValidationPipe());
+
+		await app.init();
+
+		const {
+			body: { accessToken },
+		} = await request(app.getHttpServer()).post('/auth/login').send(testAdminUser);
+		adminToken = accessToken;
+
+		const { body } = await request(app.getHttpServer()).post('/auth/login').send(testNewUser);
+		userToken = body.accessToken;
+	});
+
+	it('success - get count users', async () => {
+		return request(app.getHttpServer())
+			.get(`/users/count`)
+			.set('Authorization', 'Bearer ' + adminToken)
+			.expect(200)
+			.then(({ body }: request.Response) => {
+				expect(typeof body.total).toBe('number');
+			});
+	});
+
+	it('fail - Unauthorized (401): Unauthorized', async () => {
+		return request(app.getHttpServer())
+			.get(`/users/count`)
+			.expect(401)
+			.then(({ body }: request.Response) => {
+				expect(body.message).toBe('Unauthorized');
+			});
+	});
+
+	it('fail - Forbidden (403): user is not an admin', async () => {
+		return request(app.getHttpServer())
+			.get(`/users/count`)
+			.set('Authorization', 'Bearer ' + userToken)
+			.expect(403)
+			.then(({ body }: request.Response) => {
+				expect(body.message).toBe(AuthErrorMessages.FORBIDDEN);
+			});
+	});
+
+	it('fail - Unauthorized (401): token with fake user id', async () => {
+		const fakeId = new Types.ObjectId().toHexString();
+		const tokenWithFakeId = sign({ _id: fakeId }, process.env.JWT_SECRET_KEY);
+
+		return request(app.getHttpServer())
+			.get(`/users/count`)
+			.set('Authorization', 'Bearer ' + tokenWithFakeId)
+			.expect(401)
+			.then(({ body }: request.Response) => {
+				expect(body.message).toBe('Unauthorized');
+			});
+	});
+};
--- test/user/user.e2e-spec.ts
+++ test/user/user.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { disconnect } from 'mongoose';
+import { usersGetUsersCount } from './get-cout-users';
 
 import { usersGetProfileByToken } from './get-profile-by-token';
 import { usersGetProfileByUserId } from './get-profile-by-user-id';
@@ -11,5 +12,6 @@ afterAll(async () => {
 
 describe('users/profile (GET)', usersGetProfileByToken);
 describe('users/profile/:id (GET)', usersGetProfileByUserId);
+describe('users/count (GET)', usersGetUsersCount);
 describe('users/update (PATCH)', usersUpdate);
 describe('users/update/:id (PATCH)', usersUpdateRole);
